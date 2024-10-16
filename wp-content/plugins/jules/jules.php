aa8a740d8a9e712f5718faeb2c2ad0b21cccf2b5
Jules G. <jules.grdn@gmail.com>
2024-10-16 15:35:42 +0200

    Création du plugin avec activation

---
+++ wp-content/plugins/jules/jules.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+ * Jules
+ *
+ * @author              Jules
+ *
+ * @wordpress-plugin
+ * Plugin Name:         Jules
+ * Description:         I learn with this plugin
+ * Version:             1.0
+ * Requires PHP:        5.6
+ * Author:              Jules
+ *
+ */
+
+use Jules\Plugin\PluginTest;
+
+if (!defined('ABSPATH')) exit;
+
+define('JULES_PLUGIN_DIR', plugin_dir_path(__FILE__));
+
+require JULES_PLUGIN_DIR . 'vendor/autoload.php';
+
+$plugin = new PluginTest();
---
+++ wp-content/plugins/jules/src/PluginTest.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Jules\Plugin;
+
+class PluginTest
+{
+
+}
